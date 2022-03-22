db80205263fa929f0345d4de04dd115729696455
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-22 11:24:59 -0500

    Personalizando parametro de Circuit Breaker

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/AppConfig.java
@@ -1,9 +1,17 @@
 package com.formacionbdi.springboot.app.item;
 
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class AppConfig {
@@ -12,5 +20,24 @@ public class AppConfig {
     @LoadBalanced
     public RestTemplate registrarRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> {
+            return new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(CircuitBreakerConfig.custom()
+                            //tamaño de ventana que mide solicitudes fallidas y exitosas
+                            .slidingWindowSize(10)
+                            // porcentaje de solicitudes fallidas para entrar en estado cerrado
+                            .failureRateThreshold(50)
+                            // tiempo de espera para salir del estado cerrado
+                            .waitDurationInOpenState(Duration.ofSeconds(10L))
+                            // numero de solicitudes de prueba para salir del estado semi abierto
+                            .permittedNumberOfCallsInHalfOpenState(5)
+                            .build())
+                    .timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+                    .build();
+        });
     }
 }
