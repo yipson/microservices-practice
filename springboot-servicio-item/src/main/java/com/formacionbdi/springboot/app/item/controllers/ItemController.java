1e85c9228d0644f5856605c05579707f1caf712e
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-23 15:37:09 -0500

    Leer la configuracion desde controlador utilizando la anotacion @Value

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -9,10 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -26,6 +31,9 @@ public class ItemController {
     @Autowired
     @Qualifier("serviceFeign")
     private ItemService itemService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar(@RequestParam(name="nombre", required=false) String nombre,
@@ -86,5 +94,18 @@ public class ItemController {
         producto.setPrecio(500.00);
         item.setProducto(producto);
         return CompletableFuture.supplyAsync(() -> item);
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+
+        logger.info(puerto);
+
+        Map<String, String> json = new HashMap<>();
+
+        json.put("texto", texto);
+        json.put("puerto", puerto);
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
     }
 }
