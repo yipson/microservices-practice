798202e9cccf7b3026f621dcfd94722d407f64a6
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-25 12:51:30 -0500

    Quitando Response de text/plain y Request Header token. Cambiando el service de feign a RestTemplate y probando en postman, el controlador de eliminar esta fallando

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -35,7 +35,7 @@ public class ItemController {
     private CircuitBreakerFactory cbFactory;
 
     @Autowired
-    @Qualifier("serviceFeign")
+    @Qualifier("serviceRestTemplate")
     private ItemService itemService;
 
     @Value("${configuracion.texto}")
