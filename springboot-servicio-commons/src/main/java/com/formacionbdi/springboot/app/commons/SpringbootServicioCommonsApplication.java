a44ea69f6c654ac9f0a47b5d037a48f6aa3e8103
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-25 16:03:19 -0500

    Creando libreria commons y generando jar usando maven

---
+++ springboot-servicio-commons/src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,9 @@
+package com.formacionbdi.springboot.app.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+
+}
