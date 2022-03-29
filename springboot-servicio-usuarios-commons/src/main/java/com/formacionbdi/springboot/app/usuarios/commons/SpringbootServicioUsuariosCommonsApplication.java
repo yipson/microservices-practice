9d9cbf0e5e9b669caeb72bba44d60779e0af2454
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-29 16:07:12 -0500

    Creando libreria commons usuarios

---
+++ springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/usuarios/commons/SpringbootServicioUsuariosCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.usuarios.commons;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class SpringbootServicioUsuariosCommonsApplication {
+
+
+
+}
---
+++ springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/usuarios/commons/models/entity/Role.java
@@ -0,0 +1,36 @@
+package com.formacionbdi.springboot.app.usuarios.commons.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="roles")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 30)
+    private String nombre;
+
+    // Si se quisiera tener una relaccion bidireccional
+    /* @ManyToMany(fetch = FetchType.LAZY, mappedBy = "roles")
+    private List<Usuario> usuarios; */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
