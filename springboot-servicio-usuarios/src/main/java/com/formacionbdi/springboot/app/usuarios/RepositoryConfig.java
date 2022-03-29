6adeeb2c7cba878c4aaae8de61e624ded348b54a
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-29 17:08:18 -0500

    Modificando servicio usuarios para desacoplarlo de las entidades Usuario y Role que fueron llevadas a una libreria commons

--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
@@ -1,7 +1,7 @@
 package com.formacionbdi.springboot.app.usuarios;
 
-import com.formacionbdi.springboot.app.usuarios.models.entity.Role;
-import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Role;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -1,6 +1,6 @@
 package com.formacionbdi.springboot.app.usuarios.models.dao;
 
-import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
