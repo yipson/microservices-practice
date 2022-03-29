ccab89ff6785d9fd2f9379beed73f946cea17d3b
yipson <sebastianvalencia.ceron@gmail.com>
2022-03-29 15:12:20 -0500

    Personalizando nombre de los endpoints, acceder a estos por medio de search/.../.../, ejemplo: localhost:8090/api/usuarios/usuarios/search/obtenerPorUsername?username=andres

--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -3,12 +3,16 @@ package com.formacionbdi.springboot.app.usuarios.models.dao;
 import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path="usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
-    public Usuario findByUsername(String username);
+    // Personalizar nombre del endpoint
+    @RestResource(path="buscar-username")
+    public Usuario findByUsername(@Param("nombre") String username);
 
     // Personalizar consultas con JPQL
     @Query("select u from Usuario u where u.username=?1")
