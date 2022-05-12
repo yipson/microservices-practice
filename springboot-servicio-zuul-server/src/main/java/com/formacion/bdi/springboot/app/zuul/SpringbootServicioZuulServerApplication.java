e7b5f6b266f8580c1f74c165626e2b2ff781a8f4
yipson <sebastianvalencia.ceron@gmail.com>
2022-05-12 14:45:48 -0500

    Agregando nuevo servicio zuul

---
+++ springboot-servicio-zuul-server/src/main/java/com/formacion/bdi/springboot/app/zuul/SpringbootServicioZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.bdi.springboot.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioZuulServerApplication.class, args);
+	}
+
+}
