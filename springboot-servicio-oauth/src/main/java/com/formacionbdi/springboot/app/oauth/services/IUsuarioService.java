f925afd242020373ac962f16b5c96e6741ddfa37
yipson <sebastianvalencia.ceron@gmail.com>
2022-05-12 16:13:34 -0500

    Agregando mas informacion al token de acceso

---
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/IUsuarioService.java
@@ -0,0 +1,8 @@
+package com.formacionbdi.springboot.app.oauth.services;
+
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
+
+public interface IUsuarioService {
+
+    Usuario findByUsername(String username);
+}
--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class UsuarioService implements UserDetailsService {
+public class UsuarioService implements IUsuarioService, UserDetailsService {
 
     private Logger log = LoggerFactory.getLogger(UsuarioService.class);
 
@@ -43,5 +43,10 @@ public class UsuarioService implements UserDetailsService {
         return new User(usuario.getUsername(),
                 usuario.getPassword(),
                 usuario.getEnabled(), true, true, true, authorities);
+    }
+
+    @Override
+    public Usuario findByUsername(String username) {
+        return client.findByUsername(username);
     }
 }
