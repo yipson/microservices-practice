0e0ece86248327ca3997b59d8eedf03644ec5421
yipson <sebastianvalencia.ceron@gmail.com>
2022-05-13 16:18:28 -0500

    Manejando Eventos de exito y fracaso en la autenticacion

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.formacionbdi.springboot.app.oauth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService usuarioService;
 
+    @Autowired
+    private AuthenticationEventPublisher eventPublisher;
+
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -22,13 +26,18 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     @Autowired
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(this.usuarioService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .authenticationEventPublisher(eventPublisher);
     }
 
     @Override
     @Bean
-    protected AuthenticationManager authenticationManager() throws Exception {
+    protected AuthenticationManager authenticationManager()
+            throws Exception {
         return super.authenticationManager();
     }
 }
