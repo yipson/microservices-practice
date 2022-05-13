7319db081f14f78eca122b1e1ef1ad5f10525077
yipson <sebastianvalencia.ceron@gmail.com>
2022-05-13 13:31:15 -0500

    Configurando zuul como servidor de recursos

---
+++ springboot-servicio-zuul-server/src/main/java/com/formacion/bdi/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -0,0 +1,60 @@
+package com.formacion.bdi.springboot.app.zuul.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/security/oauth/**").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/api/productos/listar",
+                        "/api/items/listar",
+                        "/api/usuarios/usuarios/").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/api/productos/ver/{id}",
+                        "/api/items/ver/{id}/cantidad/{cantidad}",
+                        "/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.POST,
+                        "/api/productos/crear",
+                        "/api/items/crear",
+                        "/api/usuarios/usuarios/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,
+                        "/api/productos/editar/{id}",
+                        "/api/items/editar/{id}",
+                        "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,
+                        "/api/productos/eliminar/{id}",
+                        "/api/items/eliminar/{id}",
+                        "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("algun_codigo_secreto");
+        return tokenConverter;
+    }
+}
