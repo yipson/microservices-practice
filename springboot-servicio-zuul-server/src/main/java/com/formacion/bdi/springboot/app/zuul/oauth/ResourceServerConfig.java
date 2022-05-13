5f00f521e35c8bcb5c7bc3e4c38c618918a9acf8
yipson <sebastianvalencia.ceron@gmail.com>
2022-05-13 15:59:18 -0500

    Configurando Cors en spring security OAuth2

--- springboot-servicio-zuul-server/src/main/java/com/formacion/bdi/springboot/app/zuul/oauth/ResourceServerConfig.java
+++ springboot-servicio-zuul-server/src/main/java/com/formacion/bdi/springboot/app/zuul/oauth/ResourceServerConfig.java
@@ -1,9 +1,11 @@
 package com.formacion.bdi.springboot.app.zuul.oauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @RefreshScope
 @Configuration
@@ -50,7 +58,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         "/api/productos/eliminar/{id}",
                         "/api/items/eliminar/{id}",
                         "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConf = new CorsConfiguration();
+        corsConf.setAllowedOrigins(Arrays.asList("*"));
+        corsConf.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+        corsConf.setAllowCredentials(true);
+        corsConf.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConf);
+
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean =
+                new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
     @Bean
