302871219ffe811e6fc6818506653cae7950952b
yipson <sebastianvalencia.ceron@gmail.com>
2022-06-02 12:12:43 -0500

    Manejando error 404 en componente de servicio userDetailsService

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -2,6 +2,7 @@ package com.formacionbdi.springboot.app.oauth.services;
 
 import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,23 +27,28 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Usuario usuario = client.findByUsername(username);
 
-        if(usuario == null) {
+        try{
+            Usuario usuario = client.findByUsername(username);
+
+            List<GrantedAuthority> authorities = usuario.getRoles()
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getNombre()))
+                    .peek(authority -> log.info("Role: "+ authority.getAuthority()))
+                    .collect(Collectors.toList());
+            log.info("Usuario autenticado: "+username);
+
+            return new User(usuario.getUsername(),
+                    usuario.getPassword(),
+                    usuario.getEnabled(), true,
+                    true,
+                    true, authorities);
+
+        } catch(FeignException e){
             log.error("Error en el login, no existe usuario '"+username+"' en el sistema");
-            throw new UsernameNotFoundException("Error en el login, no existe usuario '"+username+"' en el sistema");
+            throw new UsernameNotFoundException("Error en el login, no existe usuario '"
+                    +username+"' en el sistema");
         }
-
-        List<GrantedAuthority> authorities = usuario.getRoles()
-                .stream()
-                .map(role -> new SimpleGrantedAuthority(role.getNombre()))
-                .peek(authority -> log.info("Role: "+ authority.getAuthority()))
-                .collect(Collectors.toList());
-        log.info("Usuario autenticado: "+username);
-
-        return new User(usuario.getUsername(),
-                usuario.getPassword(),
-                usuario.getEnabled(), true, true, true, authorities);
     }
 
     @Override
