85be4b5633cc6f13142a6a518ab1c07cd42831cc
yipson <sebastianvalencia.ceron@gmail.com>
2022-06-06 13:33:28 -0500

    Dando reglas de seguridad a las rutas de gateway

--- springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
+++ springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.gateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -11,6 +12,16 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity http) {
         return http.authorizeExchange()
+                .pathMatchers("/api/security/oauth/**").permitAll()
+                .pathMatchers(HttpMethod.GET,
+                        "/api/productos/listar",
+                        "/api/items/listar",
+                        "/api/usuarios/usuarios",
+                        "/api/items/ver/{id}/cantidad/{cantidad}",
+                        "/api/productos/ver/{id}").permitAll()
+                .pathMatchers(HttpMethod.GET, "/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+                .pathMatchers("/api/productos/**",
+                        "/api/items/**", "/api/usuarios/usuarios/**").hasRole("ADMIN")
                 .anyExchange().authenticated()
                 .and().csrf().disable()
                 .build();
