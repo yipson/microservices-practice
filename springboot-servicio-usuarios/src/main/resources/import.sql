c16358caf96e5a2e3485c43e1c88ec467a29ee3b
yipson <sebastianvalencia.ceron@gmail.com>
2022-06-09 11:16:41 -0500

    Configurando PostgreSQL en Servidor de Configuracion y agregando librearia de postgres a servicio-usuarios

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres', '$2a$10$P4sRbae9hJnsqi5MOU7oROrTmK51ZYQn.ohZ7aUsrXs0tgFj1la3S', 1, 'Andres', 'Guzman', 'profesor@mail.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$6rX5wJzpwdmUgxf.a6i.a.6hZ4ECJ5wBxmE98WS5w8z/iJ0j1if/O', 1, 'John', 'Doe', 'jhon.doe@mail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres', '$2a$10$P4sRbae9hJnsqi5MOU7oROrTmK51ZYQn.ohZ7aUsrXs0tgFj1la3S', true, 'Andres', 'Guzman', 'profesor@mail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$6rX5wJzpwdmUgxf.a6i.a.6hZ4ECJ5wBxmE98WS5w8z/iJ0j1if/O', true, 'John', 'Doe', 'jhon.doe@mail.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
